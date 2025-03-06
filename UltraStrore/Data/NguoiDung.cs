79cb8a233eb194425ef459096280778bd510cac1
quangntpk <quangntpk03714@gmail.com>
2025-03-06 18:43:23 +0700

    them_du_lieu_mau_cho_nguoi_dung

--- UltraStrore/Data/NguoiDung.cs
+++ UltraStrore/Data/NguoiDung.cs
@@ -11,6 +11,51 @@ namespace UltraStrore.Data
             DonHangMaNguoiDungNavigations = new HashSet<DonHang>();
             DonHangMaNhanVienNavigations = new HashSet<DonHang>();
             GioHangs = new HashSet<GioHang>();
+
+            MaNguoiDung = "ND001";
+            HoTen = "Nguyễn Thanh Quang";
+            NgaySinh = new DateTime(2005, 5, 15);
+            Sdt = "0367219692";
+            Cccd = "1066205015270";
+            Email = "quang@gmail.com";
+            DiaChi = "22 giải phóng, Buôn ma thuật";
+            MatKhau = "khachhang123";
+            TrangThai = 0;  // 0 Là khách hàng  1 là admin 2 là nhân viên
+            HinhAnh = "";
+            NgayTao = DateTime.Now;
+            MoTa = "Khách hàng nhiều tiền.";
+
+            var nguoiDung2 = new NguoiDung
+            {
+                MaNguoiDung = "ND002",
+                HoTen = "Triệu Văn tuệ",
+                NgaySinh = new DateTime(2001, 8, 10),
+                Sdt = "0997219692",
+                Cccd = "066205015290",
+                Email = "tue@gmail.com",
+                DiaChi = "22 tan an, Buôn ma thuat",
+                MatKhau = "nhanvien123",
+                TrangThai = 2,
+                HinhAnh = "",
+                NgayTao = DateTime.Now,
+                MoTa = "Nhân viên chăm chỉ"
+            };
+
+            var nguoiDung3 = new NguoiDung
+            {
+                MaNguoiDung = "ND003",
+                HoTen = "Ngô Quang Trung",
+                NgaySinh = new DateTime(1992, 8, 10),
+                Sdt = "0987654321",
+                Cccd = "066654321012",
+                Email = "trung@gmail.com",
+                DiaChi = "33 tay nguyen, Buôn ma thuật",
+                MatKhau = "admin123",
+                TrangThai = 1,
+                HinhAnh = "",
+                NgayTao = DateTime.Now,
+                MoTa = "Admin tốt bụng"
+            };
         }
 
         public string MaNguoiDung { get; set; } = null!;
@@ -25,6 +70,8 @@ namespace UltraStrore.Data
         public string? HinhAnh { get; set; }
         public DateTime? NgayTao { get; set; }
         public string? MoTa { get; set; }
+
+
 
         public virtual ICollection<DanhSachDiaChi> DanhSachDiaChis { get; set; }
         public virtual ICollection<DonHang> DonHangMaNguoiDungNavigations { get; set; }
